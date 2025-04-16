0037824a2122b02fa9c362d88d67f47f9c15885a
filipidalfovo <filipi.dalfovo@gmail.com>
2025-04-16 20:03:56 -0300

    Modal cadastro tarefas

---
+++ src/app/components/modal-tarefa/modal-tarefa.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-modal-tarefa',
+  standalone: false,
+  templateUrl: './modal-tarefa.component.html',
+  styleUrl: './modal-tarefa.component.css'
+})
+export class ModalTarefaComponent implements OnInit {
+
+  formGroup!: FormGroup;
+
+  constructor(
+    public matDialog: MatDialog,
+    private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.formBuilder.group({
+      descricao: ['', Validators.required, Validators.maxLength(255)],
+      concluida: [false]
+    })
+
+  }
+
+}
